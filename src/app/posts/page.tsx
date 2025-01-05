a32170a0125d1d72dea13a4f6a01f211ddc8d774
Grzegorz <19194188+farce1@users.noreply.github.com>
2025-01-05 15:02:07 +0100

    chore: write first server component and fetch data directly

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,17 @@
-export default function Posts() {
-  return <main>POSTS</main>;
+export default async function Posts() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  const data = await response.json();
+
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+      <ul>
+        {data.posts.map((post) => (
+          <li key={post.id} className="mb-5">
+            <h2 className="text-2xl font-bold">{post.title}</h2>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
